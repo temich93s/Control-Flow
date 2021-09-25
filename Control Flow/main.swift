2fbaa11b58405de5f66de828ff44a42649302708
Temich93s <nil>
2021-09-26 08:43:24 +1000

    сделано: Циклы For-in

--- Control Flow/main.swift
+++ Control Flow/main.swift
@@ -9,3 +9,39 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: Циклы For-in
+print("\n//Циклы For-in")
+
+let someArray1 = ["a", "b", "c", "d", "e"]
+
+for character in someArray1 {
+    print("Character: \(character)")
+}
+
+let someDictionary1 = [1: "one", 2: "two", 3: "three"]
+
+for (keys, values) in someDictionary1 {
+    print("keys: \(keys), values: \(values)")
+}
+
+for index in 1...5 {
+    print("\(index) * 5 = \(index * 5)")
+}
+
+for _ in 1..<4 {
+    print("Hello!")
+}
+
+let minutes = 60
+let minuteInterval = 5
+
+for tickMark in stride(from: 0, to: minutes, by: 5) {
+    print(tickMark, terminator: " ")
+}
+print()
+
+for tickMark in stride(from: 0, through: minutes, by: minuteInterval) {
+    print(tickMark, terminator: " ")
+}
+print()
